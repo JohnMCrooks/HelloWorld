97af162e125984930e65b34c99e05d906bbf62c7
John Crooks <crooks5001@gmail.com>
2016-05-16 12:13:25 -0400

    Initial Import - Week 1 Day 1

---
+++ src/Animal.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2016.
+ */
+
+/**
+ * Created by johncrooks on 5/16/16.
+ */
+public class Animal {
+    String name;
+    String species;
+    double weight;
+}
+
---
+++ src/Person.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2016.
+ */
+
+/**
+ * Created by johncrooks on 5/16/16.
+ */
+public class Person {
+    String name;
+    int age;
+    boolean isAlive;
+    Person[] family;
+}
